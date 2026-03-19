251da97b7eef120b9a76151cd8ea9b1b3a00cea7
frtzhahn <aldrinalciso2@gmail.com>
2026-03-19 18:31:48 +0800

    refactor: renamed project domain and modified the mainapp.java, modules-info.java, pom.xml

---
+++ src/main/java/com/randfacts/MainController.java
@@ -0,0 +1,14 @@
+package com.randfacts;
+
+import javafx.fxml.FXML;
+
+public class MainController{
+	@FXML
+	public void initialize(){
+
+		System.out.println("hello from mocha");
+
+
+
+	}
+} 
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,12 +1,12 @@
-module com.mocha{
+module com.randfacts{
 	//to make JVM run these libraries with the project source code
 	requires javafx.controls;
 	requires javafx.fxml;
 
 	//gives permission to the library to get in the source code directory com.mocha
-	opens com.mocha to javafx.fxml;
+	opens com.randfacts to javafx.fxml;
 
 	//allows javafx library to see my source code for this project
 	//so the library can communicate in the source code
-	exports com.mocha;
+	exports com.randfacts;
 }
