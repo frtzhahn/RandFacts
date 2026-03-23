54b3afba6b21d36e5b4c1caf4dcfac1d9de7dd06
frtzhahn <aldrinalciso2@gmail.com>
2026-03-23 20:37:06 +0800

    feat: added new fxml files, added nav bar, and fixed font issues what a pain

--- src/main/java/com/randfacts/MainController.java
+++ src/main/java/com/randfacts/MainController.java
@@ -6,7 +6,7 @@ public class MainController{
 	@FXML
 	public void initialize(){
 
-		System.out.println("hello from mocha");
+		System.out.println("\u001b[32mRandFacts Project: maven build success on your machine\u001b[0m");
 
 
 
